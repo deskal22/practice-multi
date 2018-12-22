1e09d1a1a03b921c221d013d4a222185e9184057
deskal22 <deskal22@gmail.com>
2018-12-22 15:03:34 +0000

    Created simple question

--- app.c
+++ app.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    printf("Practice Multiplication");
+    int answer;
+
+    printf("Practice Multiplication\n");
+    printf("How much is 2 x 2?\n");
+    scanf("%d", &answer);
+
+    if (answer == 4){
+        printf("Correct!\n");
+    }
+    else printf("Wrong answer..\n");
     return 0;
 }
